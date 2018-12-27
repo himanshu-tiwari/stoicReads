883228bbe13de00ebfdfabac55bfa00759f110cb
himanshu-tiwari <htiwarih@gmail.com>
2018-12-27 16:35:10 +0530

    Connected to Mlab db

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect to mlab db
+mongoose.connect('mongodb://root:Abcd1234@ds013579.mlab.com:13579/stoic-reads', { useNewUrlParser : true });
+mongoose.connection.once('open', () => {
+    console.log('connected to db!');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
