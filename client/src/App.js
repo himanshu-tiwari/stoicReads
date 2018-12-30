c4a5296d8223263a0eef8eba2c24e717ec3f9e5d
himanshu-tiwari <htiwarih@gmail.com>
2018-12-30 13:41:36 +0530

    Setup the initial component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+// components
+import BookList from './components/BookList';
+
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<h3>StoicReads - A Sacred Collection</h3>
+
+				<BookList />
 			</div>
 		);
 	}
---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BookList = (props) => {
+    return(
+        <div>
+            <ul id="book-list">
+                <li>Booky Booky</li>
+            </ul>
+        </div>
+    );
+};
+
+export default BookList;
