ad61f82c8f472228090b550a9efdf87f09dd40ec
himanshu-tiwari <htiwarih@gmail.com>
2018-12-30 12:33:17 +0530

    Set up the basics

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<h3>StoicReads - A Sacred Collection</h3>
+			</div>
+		);
+	}
 }
 
 export default App;
